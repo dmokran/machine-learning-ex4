7791e9eabc47fa26d348e1137bf91505f8807385
dmokran <dmokran@gmail.com>
2016-10-05 16:50:31 +0200

    Errata fixed
    Fixed all known errors from errata

--- ex4/randInitializeWeights.m
+++ ex4/randInitializeWeights.m
@@ -16,7 +16,7 @@ W = zeros(L_out, 1 + L_in);
 % Instructions: Initialize W randomly so that we break the symmetry while
 %               training the neural network.
 %
-% Note: The first row of W corresponds to the parameters for the bias units
+% Note: The first column of W corresponds to the parameters for the bias unit
 %
 
 
